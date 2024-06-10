d48780f403359c4f362c790244c79dfdf5f2f2d9
Alejandra Elsin <ale.elsin92@gmail.com>
2024-06-10 16:07:58 +0200

    update files

---
+++ src/TestComponent.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function TestComponent() {
+  return (
+      <div>
+          <h1>Hello from YourComponent!</h1>
+      </div>
+  );
+}
+
+export default TestComponent;
